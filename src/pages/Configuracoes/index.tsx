7f5aa11369cf8e4bce451faea21c8cb01faf3dc1
renato <neo_rlm@hotmail.com>
2024-10-19 07:11:30 -0300

    cssconfi

--- src/pages/Configuracoes/index.tsx
+++ src/pages/Configuracoes/index.tsx
@@ -1,6 +1,7 @@
 import { Settings } from '@mui/icons-material'
-import { Box, Tab, Tabs, Toolbar } from '@mui/material'
-import React from 'react'
+import { Box, Checkbox, List, ListItem, Tab, Tabs, Toolbar, Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { useAuth } from '../../context/AuthContext'
 
 interface TabPanelProps {
   children?: React.ReactNode
@@ -31,11 +32,45 @@ function a11yProps(index: number) {
   }
 }
 export const Configuracoes = () => {
+  const { decryptData } = useAuth()
   const [value, setValue] = React.useState(0)
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue)
   }
+  const confi  = JSON.parse(localStorage.getItem("configuracoes"))
+  const [configuracoesOpcoes, setConfiguracoesOpcoes] = useState([])
+
+  const usuario =  JSON.parse(decryptData('usuario'))
+  useEffect(()=>{
+    if(confi) {
+      setConfiguracoesOpcoes(confi)
+      console.log(confi, usuario.tenantId)
+    }
+  },[setConfiguracoesOpcoes])
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [checked, setChecked] = useState<{
+    [key: string] : {
+        checked: boolean
+    }
+  }>({});
+
+  const handleChangeCheck = (event: React.ChangeEvent<HTMLInputElement>, key: string) => {
+    const value = event.target.checked
+    console.log(value)
+    setChecked(prevState => ({
+      ...prevState,
+      [key]: { checked: value },
+    }))
+  };
+  const handleListItemClick = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    index: number,
+  ) => {
+    if(index === selectedIndex) return
+    setSelectedIndex(index);
+    console.log(checked)
+  };
 
   return (
     <Box sx={{ width: '100%', maxWidth: { sm: '100%', md: '1700px' }, pt: 2 }}>
@@ -45,13 +80,38 @@ export const Configuracoes = () => {
           onChange={handleChange}
           aria-label="basic tabs example"
         >
-          <Tab label="Item One" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
+          <Tab label="Configuracoes Gerais" {...a11yProps(0)} icon={<Settings/>} /> 
+          {/* <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} /> */}
         </Tabs>
       </Box>
       <CustomTabPanel value={value} index={0}>
-        Item One
+        {/* <Box  sx={{display: "flex", flexDirection: 'column', justifyContent: "space-between"}}> */}
+        {configuracoesOpcoes
+       .filter(id => id.tenantId === usuario.tenantId)
+        .map(item => (
+          <List key={item.id} component="nav" aria-label="main mailbox folders" 
+         >
+            <ListItem 
+             sx={{display: "flex", flexDirection: 'row', justifyContent: "space-between", width: '100%'}}
+            onClick={(event) => handleListItemClick(event, item.id)}
+            >
+              <Typography variant='body2'>Desc</Typography> 
+            <Box>
+              {(item.value === "disabled" || item.value === "enabled") ? (
+          <Checkbox 
+            checked={checked[item.id]?.checked || false}  // Usa o estado para controlar o checkbox
+            onChange={e => handleChangeCheck(e, item.id)}
+            inputProps={{ 'aria-label': 'controlled' }}
+          />
+        ) : (
+          <Typography variant='body2'>{item.value}</Typography>  // Mostra o valor quando não for 'disabled' ou 'enabled'
+        )}
+        </Box>
+            </ListItem>
+            </List>
+        ))}
+        {/* </Box> */}
       </CustomTabPanel>
       <CustomTabPanel value={value} index={1}>
         Item Two
