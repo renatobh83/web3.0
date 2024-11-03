71a329344a5efa3a83ce1d4265c9ccb232f1caf6
renato <neo_rlm@hotmail.com>
2024-11-03 14:05:11 -0300

    build1

--- src/pages/Usuarios/index.tsx
+++ src/pages/Usuarios/index.tsx
@@ -1,198 +1,210 @@
-import { Close, Edit, QueueSharp } from "@mui/icons-material"
-import { Box, Button, IconButton, styled, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, TextField, Tooltip, Typography } from "@mui/material"
-import { useUsuarioStore } from "../../store/usuarios";
-import { useCallback, useEffect, useState } from "react";
-import { DeleteUsuario, ListarUsuarios } from "../../services/user";
-import { ListarFilas } from "../../services/filas";
-import { toast } from "sonner";
-import { ModalUsuario, type Usuario } from "./ModalUsuario";
-import { CustomTableContainer } from "../../components/MaterialUi/CustomTable";
-
+import { Close, Edit, QueueSharp } from '@mui/icons-material'
+import {
+  Box,
+  Button,
+  IconButton,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+  TextField,
+  Tooltip,
+  Typography,
+} from '@mui/material'
+import { useUsuarioStore } from '../../store/usuarios'
+import { useCallback, useEffect, useState } from 'react'
+import { DeleteUsuario, ListarUsuarios } from '../../services/user'
+import { ListarFilas } from '../../services/filas'
+import { toast } from 'sonner'
+import { ModalUsuario, type Usuario } from './ModalUsuario'
 
 const optionsProfile = [
-    { value: "user", label: "Usuário" },
-    { value: "super", label: "Supervisor" },
-    { value: "admin", label: "Administrador" },
-];
-
-
+  { value: 'user', label: 'Usuário' },
+  { value: 'super', label: 'Supervisor' },
+  { value: 'admin', label: 'Administrador' },
+]
 
 export const Usuarios: React.FC = () => {
-    const {
-        usuarios,
-        editarUsuario,
-        deletarUsuario,
-        toggleModalUsuario,
-        setUsuarioSelecionado,
-        loadUsuarios,
-        toggleModalFilaUsuario,
-    } = useUsuarioStore();
-    const [filter, setFilter] = useState("");
-    const [pagination, setPagination] = useState({ page: 0, rowsPerPage: 10 });
-    const [loading, setLoading] = useState(false);
-    const [filas, setFilas] = useState([]);
-    const [params, setParams] = useState({
-        pageNumber: 1,
-        searchParam: null,
-        hasMore: true,
-    });
+  const {
+    usuarios,
+    editarUsuario,
+    deletarUsuario,
+    toggleModalUsuario,
+    setUsuarioSelecionado,
+    loadUsuarios,
+    toggleModalFilaUsuario,
+  } = useUsuarioStore()
 
-    const handleFilterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setFilter(e.target.value);
-    };
+  const [filter, setFilter] = useState('')
+  const [pagination, setPagination] = useState({ page: 0, rowsPerPage: 10 })
+  const [loading, setLoading] = useState(false)
+  const [filas, setFilas] = useState([])
+  const [params, setParams] = useState({
+    pageNumber: 1,
+    searchParam: null,
+    hasMore: true,
+  })
 
-    const handlePageChange = (event: unknown, newPage: number) => {
-        setPagination((prev) => ({ ...prev, page: newPage }));
-    };
+  const handleFilterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFilter(e.target.value)
+  }
 
-    const handleRowsPerPageChange = (
-        event: React.ChangeEvent<HTMLInputElement>,
-    ) => {
-        setPagination({ page: 0, rowsPerPage: Number.parseInt(event.target.value, 10) });
-    };
+  const handlePageChange = (event: unknown, newPage: number) => {
+    setPagination(prev => ({ ...prev, page: newPage }))
+  }
 
-    const filteredUsuarios = usuarios.filter((user) =>
-        user.name.toLowerCase().includes(filter.toLowerCase()),
-    );
-    // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
-    const listarUsuarios = useCallback(async () => {
-        setLoading(true);
-        const { data } = await ListarUsuarios(params);
+  const handleRowsPerPageChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setPagination({ page: 0, rowsPerPage: parseInt(event.target.value, 10) })
+  }
 
-        loadUsuarios(data.users);
-        setParams((prev) => {
-            return {
-                ...prev,
-                hasMore: data.hasMore,
-            };
-        });
-        setLoading(false);
-    }, []);
-    const listarFilas = useCallback(async () => {
-        const { data } = await ListarFilas();
-        setFilas(data);
-    }, []);
-    // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
-    useEffect(() => {
-        listarFilas();
-        listarUsuarios();
-    }, []);
+  const filteredUsuarios = usuarios.filter(user =>
+    user.name.toLowerCase().includes(filter.toLowerCase())
+  )
 
-    const gerirFilasUsuario = (usuario: Usuario) => {
-        setUsuarioSelecionado(usuario);
-        toggleModalFilaUsuario();
-    };
-    const handlEditarUsuario = (usuario: Usuario) => {
-        setUsuarioSelecionado(usuario);
-        toggleModalUsuario();
-    };
-    const handleDeleteUsuario = (usuario: Usuario) => {
-        toast.info(
-            `Atenção!! Deseja realmente deletar o usuario "${usuario.name}"?`,
-            {
-                position: "top-center",
-                cancel: {
-                    label: "Cancel",
-                    onClick: () => console.log("Cancel!"),
-                },
-                action: {
-                    label: "Confirma",
-                    onClick: () => {
-                        DeleteUsuario(usuario.id).then(() => {
-                            toast.success("Usuario apagado", {
-                                position: "top-center",
-                            });
-                            listarUsuarios()
-                        });
-                    },
-                },
-            },
-        );
-    };
+  const listarUsuarios = useCallback(async () => {
+    setLoading(true)
+    const { data } = await ListarUsuarios(params)
+    loadUsuarios(data.users)
+    setParams(prev => ({ ...prev, hasMore: data.hasMore }))
+    setLoading(false)
+  }, [params, loadUsuarios])
 
-    return (
-        <Box sx={{ width: '100%', maxWidth: { sm: '100%', md: '1700px' }, pt: 2 }}>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', }}>
-                <Typography variant="h6">Usuarios</Typography>
-                <TextField
-                    label="Localize"
-                    variant="standard"
-                    size="small"
-                    sx={{ width: { xs: '200px', sm: '320px' } }}
-                    value={filter}
-                    onChange={handleFilterChange}
-                />
-                <Button
-                    variant="contained"
-                    color="secondary"
-                    onClick={() => {
-                        setUsuarioSelecionado(null);
-                        toggleModalUsuario();
-                    }}
-                >
-                    Adicionar
-                </Button>
-            </Box>
-            <CustomTableContainer sx={{ mt: 2, }}>
-                <TableHead>
-                    <TableRow >
-                        <TableCell>ID</TableCell>
-                        <TableCell>Nome</TableCell>
-                        <TableCell>Perfil</TableCell>
-                        <TableCell sx={{ textAlign: 'center' }}>Ações</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {filteredUsuarios
-                        .slice(
-                            pagination.page * pagination.rowsPerPage,
-                            pagination.page * pagination.rowsPerPage +
-                            pagination.rowsPerPage,
-                        )
-                        .map((usuario) => (
-                            <TableRow key={usuario.id}>
-                                <TableCell>{usuario.id}</TableCell>
-                                <TableCell>{usuario.name}</TableCell>
-                                <TableCell>{usuario.profile}</TableCell>
-                                <TableCell>
-                                    <Box sx={{ display: 'flex', justifyContent: 'center', gap: 1 }} className="flex space-x-2 justify-center">
-                                        <Tooltip title="Gestão de Filas do usuário">
-                                            <IconButton
-                                                onClick={() => gerirFilasUsuario(usuario)}
-                                            >
-                                                <QueueSharp />
-                                            </IconButton>
-                                        </Tooltip>
-                                        <Tooltip title="Editar usuário">
-                                            <IconButton
-                                                onClick={() => handlEditarUsuario(usuario)}
-                                            >
-                                                <Edit />
-                                            </IconButton>
-                                        </Tooltip>
-                                        <Tooltip title="Apagar usuário">
-                                            <IconButton
-                                                onClick={() => handleDeleteUsuario(usuario)}
-                                            >
-                                                <Close />
-                                            </IconButton>
-                                        </Tooltip>
-                                    </Box>
-                                </TableCell>
-                            </TableRow>
-                        ))}
-                </TableBody>
+  const listarFilas = useCallback(async () => {
+    const { data } = await ListarFilas()
+    setFilas(data)
+  }, [])
 
-            </CustomTableContainer>
-            <TablePagination
-                component="div"
-                count={filteredUsuarios.length}
-                page={pagination.page}
-                onPageChange={handlePageChange}
-                rowsPerPage={pagination.rowsPerPage}
-                onRowsPerPageChange={handleRowsPerPageChange}
-            />
+  useEffect(() => {
+    listarFilas()
+    listarUsuarios()
+  }, [listarFilas, listarUsuarios])
 
-        </Box >
+  const gerirFilasUsuario = (usuario: Usuario) => {
+    setUsuarioSelecionado(usuario)
+    toggleModalFilaUsuario()
+  }
+
+  const handleEditarUsuario = (usuario: Usuario) => {
+    setUsuarioSelecionado(usuario)
+    toggleModalUsuario()
+  }
+
+  const handleDeleteUsuario = (usuario: Usuario) => {
+    toast.info(
+      `Atenção!! Deseja realmente deletar o usuario "${usuario.name}"?`,
+      {
+        position: 'top-center',
+        cancel: {
+          label: 'Cancelar',
+          onClick: () => console.log('Cancelado!'),
+        },
+        action: {
+          label: 'Confirmar',
+          onClick: () => {
+            DeleteUsuario(usuario.id).then(() => {
+              toast.success('Usuário apagado', { position: 'top-center' })
+              listarUsuarios()
+            })
+          },
+        },
+      }
     )
+  }
+
+  return (
+    <Box sx={{ width: '100%', maxWidth: { sm: '100%', md: '1700px' }, pt: 2 }}>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant="h6">Usuários</Typography>
+        <TextField
+          label="Localizar"
+          variant="standard"
+          size="small"
+          sx={{ width: { xs: '200px', sm: '320px' } }}
+          value={filter}
+          onChange={handleFilterChange}
+        />
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() => {
+            setUsuarioSelecionado(null)
+            toggleModalUsuario()
+          }}
+        >
+          Adicionar
+        </Button>
+      </Box>
+      <TableContainer component={Box} sx={{ mt: 2 }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>Nome</TableCell>
+              <TableCell>Perfil</TableCell>
+              <TableCell sx={{ textAlign: 'center' }}>Ações</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {filteredUsuarios
+              .slice(
+                pagination.page * pagination.rowsPerPage,
+                pagination.page * pagination.rowsPerPage +
+                  pagination.rowsPerPage
+              )
+              .map(usuario => (
+                <TableRow key={usuario.id}>
+                  <TableCell>{usuario.id}</TableCell>
+                  <TableCell>{usuario.name}</TableCell>
+                  <TableCell>{usuario.profile}</TableCell>
+                  <TableCell>
+                    <Box
+                      sx={{ display: 'flex', justifyContent: 'center', gap: 1 }}
+                    >
+                      <Tooltip title="Gestão de Filas do usuário">
+                        <IconButton onClick={() => gerirFilasUsuario(usuario)}>
+                          <QueueSharp />
+                        </IconButton>
+                      </Tooltip>
+                      <Tooltip title="Editar usuário">
+                        <IconButton
+                          onClick={() => handleEditarUsuario(usuario)}
+                        >
+                          <Edit />
+                        </IconButton>
+                      </Tooltip>
+                      <Tooltip title="Apagar usuário">
+                        <IconButton
+                          onClick={() => handleDeleteUsuario(usuario)}
+                        >
+                          <Close />
+                        </IconButton>
+                      </Tooltip>
+                    </Box>
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        component="div"
+        count={filteredUsuarios.length}
+        page={pagination.page}
+        onPageChange={handlePageChange}
+        rowsPerPage={pagination.rowsPerPage}
+        onRowsPerPageChange={handleRowsPerPageChange}
+      />
+    </Box>
+  )
 }
