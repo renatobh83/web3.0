496855e7ac8d431b58cb8337308bbe9c20e6cfdd
Renato <neo_rlm@hotmail.com>
2024-11-08 10:18:46 -0300

    erroBuild

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.tsx'
 import { App } from './App.tsx'
-import './index.css'
+
 
 import AppTheme from './Theme/AppTheme.tsx'
 
